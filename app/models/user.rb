dce4a085ad73b26089843f39b075bee7be6aba3f
Mathew Roberts <mathewroberts@mathews-air.ait-sydney.net.au>
2018-04-23 09:59:27 +1000

    follow added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :liked_posts, through: :likes, source: :post
+
+  has_many :follows, foreign_key: 'followed_id'
+  has_many :followers, through: :follows#, source: :followers
+
+  has_many :reverse_follows, class_name: 'Follow', foreign_key: 'follower_id'
+  has_many :followees, through: :reverse_follows#, source: :followees
+
 end
